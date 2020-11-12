1397294b845b08a602b6e7fe5189e1c9b328a1a8
Henry <hi@henry.wang>
2020-11-12 00:16:59 +0000

    feat: add data helper

--- src/util/helper.js
+++ src/util/helper.js
@@ -1,3 +1,40 @@
+const fs = require("fs");
+const path = require("path");
+const parse = require("csv-parse");
 
-module.exports = {
+const write = (name, content) => {
+  const p = path.resolve(__dirname, `../../public/assets/data/out/${name.age_group}`);
+  fs.mkdirSync(p, { recursive: true }, (err) => {
+    if (err) throw err;
+  });
+
+  let writer = fs.createWriteStream(`${p}/${name.iter}.csv`, {
+    flags: "w",
+  });
+  writer.write(content);
+  writer.end();
 };
+
+const calculateMedian = (iter = 200, age_group = 8) => {
+  let parser = parse({ columns: true, trim: true, cast: true }, function (err, data) {
+    for (let j = 0; j < age_group; j++) {
+      for (let i = 0; i < iter; i++) {
+        console.log(`Processing age group: ${j}, iteration: ${i}`);
+
+        let res = "H,D,R\r\n";
+
+        data
+          .filter((f) => f.iter === i && f.age_group === j)
+          .forEach((f) => {
+            res += `${f.H},${f.D},${f.R}\r\n`;
+          });
+
+        write({ iter: i, age_group: j }, res);
+      }
+    }
+  });
+
+  fs.createReadStream(path.resolve(__dirname, "../../public/assets/output1 copy.csv")).pipe(parser);
+};
+
+calculateMedian();
